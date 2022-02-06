e05ae8b2c8386af1eb5999ad1c471d87bbd43023
ADEEL FAROOQ <60884333+adeel-farooq@users.noreply.github.com>
2022-02-06 18:48:36 +0500

    search

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,20 +2,20 @@ const mongoose  = require("mongoose");
 
 const QuestionModel = mongoose.model("Question")
 
-
-
 exports.getQuestions = async (req,res)=>{
-    QuestionModel.find((err,docs)=>{
-    
-if(!err){
+    const {word}=req.query;
+ var docs=await   QuestionModel.find({Question: { $regex: '.*' + word + '.*' } })
+  
+if(docs.length){
 
     res.status(200).json({ message: 'Records SuccessFully Found', hasError: false,items:docs });
 }else{
-    res.status(200).json({ message: 'No Recode', hasError: true,items:docs });
+    res.status(200).json({ message: 'No Recode', hasError: false,items:docs });
 }
-    })
+  
 
 }
+
 
 exports.postQuestion= async (req,res)=>{
     const {Question}= req.body;
--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 // const mongoConnection =`mongodb://localhost:27017/pencil` 
-const mongoConnection ="mongodb+srv://mongodb:pencil123@cluster0.9uee2.mongodb.net/pencil?retryWrites=true&w=majority"
+const mongoConnection ="mongodb+srv://pencil:pencil123@cluster0.ila2z.mongodb.net/pencil"
 mongoose.connect(mongoConnection, { useNewUrlParser: true }, (error)=>{
     if(!error){
         console.log('success');
